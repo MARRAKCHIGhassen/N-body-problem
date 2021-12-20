bf6a91534f6c5bb301515f18bdeffb69868a44bf
MARRAKCHI Ghassen <43677888+MARRAKCHIGhassen@users.noreply.github.com>
2021-12-20 17:12:31 +0100

    initialisation

---
+++ launch_simulation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+# Vérification des arguments
+
+
+# Vérification de conda
+echo "${RED} Initialisation de conda ${NC}"
+
+# Vérification et activation de l'existence de l'environnement
+echo "${RED} Vérification et activation de l'existence de l'environnement ${NC}"
+
+# Exécution du script
+echo "${RED} Exécution du script ${NC}"
+/home/marrakchi/anaconda3/envs/NBody-Env/bin/python NBody-problem.py
